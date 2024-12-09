09da98c1be573671e3688a96f947978d93865e8a
David Rapan <david@rapan.cz>
2024-12-09 23:19:39 +0100

    refactor: init

--- pysolarmanv5/__init__.py
+++ pysolarmanv5/__init__.py
@@ -1,10 +1,8 @@
 """This is a Python module to interact with Solarman (IGEN-Tech) v5 based solar
 inverter data loggers"""
 
-from pysolarmanv5.pysolarmanv5 import PySolarmanV5
-from pysolarmanv5.pysolarmanv5_async import PySolarmanV5Async
-from pysolarmanv5.pysolarmanv5 import V5FrameError
-from pysolarmanv5.pysolarmanv5 import NoSocketAvailableError
+from .pysolarmanv5 import PySolarmanV5, V5FrameError, NoSocketAvailableError
+from .pysolarmanv5_async import PySolarmanV5Async
 
 name = "pysolarmanv5"  # pylint: disable=invalid-name (C0103)
 
