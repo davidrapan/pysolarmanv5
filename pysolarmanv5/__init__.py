aa7f5d8d35676f73e137bcb42a51f67b5018098c
David Rapan <david@rapan.cz>
2024-12-22 04:23:53 +0100

    feat: Add PySolarman which handles ethernet loggers

--- pysolarmanv5/__init__.py
+++ pysolarmanv5/__init__.py
@@ -3,10 +3,12 @@ inverter data loggers"""
 
 from .pysolarmanv5 import PySolarmanV5, V5FrameError, NoSocketAvailableError
 from .pysolarmanv5_async import PySolarmanV5Async
+from .pysolarman import PySolarman
 
 name = "pysolarmanv5"  # pylint: disable=invalid-name (C0103)
 
 __all__ = [
+    "PySolarman"
     "PySolarmanV5",
     "PySolarmanV5Async",
     "V5FrameError",
---
+++ pysolarmanv5/pysolarman.py
@@ -0,0 +1,116 @@
+"""pysolarman.py"""
+
+import types
+import struct
+
+from umodbus.client.tcp import read_coils, read_discrete_inputs, read_holding_registers, read_input_registers, write_single_coil, write_multiple_coils, write_single_register, write_multiple_registers, parse_response_adu
+
+from .pysolarmanv5_async import PySolarmanV5Async
+
+
+CONTROL_CODE = types.SimpleNamespace()
+CONTROL_CODE.REQUEST = struct.pack("<H", 0x4510)
+CONTROL_CODE.RESPONSE = struct.pack("<H", 0x1510)
+CONTROL_CODE.HEARTBEAT = struct.pack("<I", 0x47100001)
+CONTROL_CODE.HEARTBEAT_RESPONSE = struct.pack("<H", 0x1710)
+
+def is_ethernet_frame(frame):
+    if frame[3:5] == CONTROL_CODE.REQUEST and (frame_len := len(frame)):
+        if frame_len > 9:
+            return int.from_bytes(frame[5:6], byteorder = "big") == len(frame[6:]) and int.from_bytes(frame[8:9], byteorder = "big") == len(frame[9:])
+        if frame_len > 6: # [0xa5, 0x17, 0x00, 0x10, 0x45, 0x03, 0x00, 0x98, 0x02]
+            return int.from_bytes(frame[5:6], byteorder = "big") == len(frame[6:])
+    return False
+
+def mb_compatibility(response, request):
+    return response if not 8 <= (l := len(response)) <= 10 else response[:5] + b'\x06' + response[6:] + (request[l:10] if len(request) > 12 else (b'\x00' * (10 - l))) + b'\x00\x01'
+
+class PySolarmanV5AsyncWrapper(PySolarmanV5Async):
+    def __init__(self, address, serial, port, mb_slave_id, logger, auto_reconnect, socket_timeout):
+        super().__init__(address, serial, port = port, mb_slave_id = mb_slave_id, logger = logger, auto_reconnect = auto_reconnect, socket_timeout = socket_timeout)
+
+    @property
+    def auto_reconnect(self):
+        return self._needs_reconnect
+
+    async def connect(self) -> bool:
+        if not self.reader_task:
+            self.logger.info(f"[{self.serial}] Connecting to {self.address}:{self.port}")
+            await super().connect()
+            return True
+        return False
+
+    async def disconnect(self) -> None:
+        self.logger.info(f"[{self.serial}] Disconnecting from {self.address}:{self.port}")
+        try:
+            await super().disconnect()
+        finally:
+            self.reader_task = None
+            self.reader = None
+            self.writer = None
+
+class PySolarmanAsync(PySolarmanV5AsyncWrapper):
+    def __init__(self, address, serial, port, mb_slave_id, logger, auto_reconnect, socket_timeout):
+        super().__init__(address, serial, port, mb_slave_id, logger, auto_reconnect, socket_timeout)
+        self._passthrough = False
+
+    async def _tcp_send_receive_frame(self, mb_request_frame):
+        return mb_compatibility(await self._send_receive_v5_frame(mb_request_frame), mb_request_frame)
+
+    async def _tcp_parse_response_adu(self, mb_request_frame):
+        return parse_response_adu(await self._tcp_send_receive_frame(mb_request_frame), mb_request_frame)
+
+    def _received_frame_is_valid(self, frame):
+        if self._passthrough:
+            return True
+        if not frame.startswith(self.v5_start):
+            self.log.debug("[%s] V5_MISMATCH: %s", self.serial, frame.hex(" "))
+            return False
+        if frame[5] != self.sequence_number and is_ethernet_frame(frame):
+            self.log.debug("[%s] V5_ETHERNET_DETECTED: %s", self.serial, frame.hex(" "))
+            self._passthrough = True
+            return True
+        if frame[5] != self.sequence_number:
+            self.log.debug("[%s] V5_SEQ_NO_MISMATCH: %s", self.serial, frame.hex(" "))
+            return False
+        return True
+
+    async def read_coils(self, register_addr, quantity):
+        if not self._passthrough:
+            return await super().read_coils(register_addr, quantity)
+        return await self._tcp_parse_response_adu(read_coils(self.mb_slave_id, register_addr, quantity))
+
+    async def read_discrete_inputs(self, register_addr, quantity):
+        if not self._passthrough:
+            return await super().read_discrete_inputs(register_addr, quantity)
+        return await self._tcp_parse_response_adu(read_discrete_inputs(self.mb_slave_id, register_addr, quantity))
+
+    async def read_input_registers(self, register_addr, quantity):
+        if not self._passthrough:
+            return await super().read_input_registers(register_addr, quantity)
+        return await self._tcp_parse_response_adu(read_input_registers(self.mb_slave_id, register_addr, quantity))
+
+    async def read_holding_registers(self, register_addr, quantity):
+        if not self._passthrough:
+            return await super().read_holding_registers(register_addr, quantity)
+        return await self._tcp_parse_response_adu(read_holding_registers(self.mb_slave_id, register_addr, quantity))
+
+    async def write_single_coil(self, register_addr, value):
+        if not self._passthrough:
+            return await super().write_single_coil(register_addr, value)
+        return await self._tcp_parse_response_adu(write_single_coil(self.mb_slave_id, register_addr, value))
+
+    async def write_multiple_coils(self, register_addr, values):
+        if not self._passthrough:
+            return await super().write_multiple_coils(register_addr, values)
+        return await self._tcp_parse_response_adu(write_multiple_coils(self.mb_slave_id, register_addr, values))
+
+    async def write_holding_register(self, register_addr, value):
+        if not self._passthrough:
+            return await super().write_holding_register(register_addr, value)
+        return await self._tcp_parse_response_adu(write_single_register(self.mb_slave_id, register_addr, value))
+
+    async def write_multiple_holding_registers(self, register_addr, values):
+        if not self._passthrough:
+            return await super().write_multiple_holding_registers(register_addr, values)
+        return await self._tcp_parse_response_adu(write_multiple_registers(self.mb_slave_id, register_addr, values))
